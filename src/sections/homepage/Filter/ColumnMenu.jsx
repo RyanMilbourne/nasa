150bd671373eddd9c345e9218fc3081b34291a18
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-27 13:59:16 -0700

    refactor column menu and styling

--- src/sections/homepage/Filter/ColumnMenu.jsx
+++ src/sections/homepage/Filter/ColumnMenu.jsx
@@ -1,24 +1,34 @@
 import React, { useContext } from "react";
 import "./ColumnMenuStyles.scss";
 import { MarsContext } from "../../../context/marsRoverContext";
+import RectangleRoundedIcon from "@mui/icons-material/RectangleRounded";
 
 const ColumnMenu = () => {
-  const { handleColumnChange } = useContext(MarsContext);
+  const { handleColumnChange, columns } = useContext(MarsContext);
+
+  const columnValues = [1, 2, 3, 4];
+
+  const iconStyle = {
+    transform: "rotateZ(90deg) scaleX(1.5)",
+    width: "1rem",
+  };
+
   return (
     <div className="column-menu-container">
       <div className="column-menu-wrapper">
-        <div className="column-option" onClick={() => handleColumnChange(1)}>
-          1
-        </div>
-        <div className="column-option" onClick={() => handleColumnChange(2)}>
-          2
-        </div>
-        <div className="column-option" onClick={() => handleColumnChange(3)}>
-          3
-        </div>
-        <div className="column-option" onClick={() => handleColumnChange(4)}>
-          4
-        </div>
+        {columnValues.map((numColumns) => (
+          <div
+            key={numColumns}
+            className={`column-option ${
+              columns === numColumns ? "currentSelection" : ""
+            }`}
+            onClick={() => handleColumnChange(numColumns)}
+          >
+            {Array.from({ length: numColumns }, (_, index) => (
+              <RectangleRoundedIcon key={index} style={iconStyle} />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
