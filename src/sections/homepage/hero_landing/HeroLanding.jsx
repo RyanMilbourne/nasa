248f2295e82255b3288cc00f58afee602eb5b6f4
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-27 09:34:34 -0700

    update landing styles

--- src/sections/homepage/hero_landing/HeroLanding.jsx
+++ src/sections/homepage/hero_landing/HeroLanding.jsx
@@ -4,7 +4,8 @@ import "./HeroLandingStyles.scss";
 const HeroLanding = () => {
   return (
     <div className="hero-landing-container">
-      <div className="hero-landing-wrapper"></div>
+      <div className="hero-landing-wrapper" />
+      <div className="hero-landing-graphic" />
     </div>
   );
 };
