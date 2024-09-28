5e8bb607aed2bc1d8ae31ee3d44a9f9703e8a90b
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-28 11:45:15 -0700

    cleanup and remove unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./sections/navbar/Navbar";
 import HomeRoute from "./routes/HomeRoute";
-import Footer from "./sections/footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
         <HomeRoute />
-        <Footer />
       </BrowserRouter>
     </>
   );
