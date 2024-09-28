df829612c74f8369c42fbcaf06224f0e09952503
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-09-28 13:42:41 -0700

    add title and styling

--- src/sections/homepage/hero_landing/HeroLanding.jsx
+++ src/sections/homepage/hero_landing/HeroLanding.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./HeroLandingStyles.scss";
+import CuriosityTitle from "../../../assets/CuriosityTitle";
 
 const HeroLanding = () => {
   return (
     <div className="hero-landing-container">
       <div className="hero-landing-wrapper">
         {/* <div className="hero-title-wrapper">greetings, earthling</div> */}
+      </div>
+      <div className="hero-landing-title-container">
+        <CuriosityTitle />
       </div>
       <div className="hero-landing-graphic" />
       <div className="hero-landing-dust" />
